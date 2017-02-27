058e0441c21ddab34140cae7833597d0daa1f924
PamelaKelly <pamelakelly7@gmail.com>
2017-02-27 13:46:07 +0000

    Initial commit - basic project skeleton & tests skeleton

---
+++ src/tests/tests.py
@@ -0,0 +1,37 @@
+"""This is the set of tests - written in advance of the program 
+to test the accuracy and functionality of the lightswitch program"""
+
+#test 1 - does the program access the file correctly. 
+
+def file_handling_test():
+    pass
+
+#test 2 - does the program correctly identify and implement the grid size.
+
+def grid_size_test():
+    pass
+
+#test 3 - does the program read commands correctly. 
+
+def command_parsing_test():
+    pass
+
+#test 4 - does the program implement the commands correctly. 
+
+def switching_lights_test():
+    pass
+
+#test 5 - does the program deal with invalid coordinates appropriately. 
+
+def outside_limits_test():
+    pass
+
+#test 6 - does the program count the number of lights on correctly. 
+
+def count_lights_test():
+    pass
+
+#test 7 - does the program correctly identify the lights that are on
+
+def which_lights_on_test():
+    pass
